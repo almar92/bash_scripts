64777c74b50f226d11fabed179b68a01c9b1e31b
squar3wave <squar3wave@gmail.com>
2023-11-29 12:06:16 +0100

    tidying up

--- vnc_double_screen/vnc_double_screen.sh
+++ vnc_double_screen/vnc_double_screen.sh
@@ -10,23 +10,23 @@ x_base=1280
 y_base=800
 
 
-if [ $1 == "vertical" ]
+if [ "$1" == "vertical" ]
 then
   # max 10:16 aspect ratio obtainable on my machine
   xrandr --output HDMI-1-1 --mode "${x_base}x${y_base}" --rotate right
   y_base=1280
   x_base=800
-elif [ $1 == "horizontal" ]
+elif [ "$1" == "horizontal" ]
 then
   # max 16:10 aspect ratio obtainable on my machine
   xrandr --output HDMI-1-1 --mode "${x_base}x${y_base}" --rotate normal
-elif [ -z $1 ]
+elif [ -z "$1" ]
 then
   echo "ERROR! Missing input"
-  echo "usage: ./vnclol.sh [vertical/horizontal] [below/right] [x_scale y_scale](optional)"
+  echo "usage: ./vnc_double_screen.sh [vertical/horizontal] [below/right] [x_scale y_scale](optional)"
   echo "If no scaling selected the option 1.5x1.5 will be set, resulting in 1920x1200 resolution"
   exit
-elif [ -z $2 ]
+elif [ -z "$2" ]
 then
   echo "ERROR! Missing screen position, choose between [below/right]"
   exit
@@ -36,7 +36,7 @@ fi
 # 1.25x1.25 gives 1000x1600
 # 1.50x1.50 gives 1200x1920
 
-if [ -z $3 ]
+if [ -z "$3" ]
 then
   echo "at least one scaling parameter not specified: setting to 1.5x1.5"
   x_scale=1.50
@@ -44,7 +44,7 @@ then
 else
   x_scale=$3
   y_scale=$4
-    if [ -z $4 ]
+    if [ -z "$4" ]
     then
       echo "at least one scaling parameter not specified: setting to 1.5x1.5"
       x_scale=1.50
@@ -66,12 +66,12 @@ y=${y_bc%.*}
 # xrandr applies scaling
 xrandr --output HDMI-1-1 --scale ${x_scale}x${y_scale}
 
-if [ $2 == "below" ]
+if [ "$2" == "below" ]
 then
   xrandr --output HDMI-1-1 --below eDP-1-1
   nitrogen --restore
   x11vnc -clip "${x}x${y}"+0+1080 -localhost -multiptr -ncache 10
-  elif [ $2 == "right" ]
+  elif [ "$2" == "right" ]
 then
   xrandr --output HDMI-1-1 --right-of eDP-1-1
   nitrogen --restore
